44faec7d7463559f5c956d24ee7c38cd7ebb01d0
Aryaan Raza <aryaan.raza.dev@gmail.com>
2026-08-01 00:16:01 +0530

    refactor(avatar): centralize companion collection metadata

--- js/data/companion.js
+++ js/data/companion.js
@@ -7,44 +7,170 @@ const COMPANIONS = [
     id: "basic",
     name: "Streaksaur Prime",
     image: "../img/avatars/streaksaur/basic.png",
-
     collection: "starter",
     rarity: "Common",
-    class: "Guardian",
+    unlockStreak: 0,
+  },
 
-    unlockType: "starter",
-    unlockValue: 0,
+  {
+    id: "sleepy",
+    name: "Sleepy Streaksaur",
+    image: "../img/avatars/streaksaur/sleepy.png",
+    collection: "starter",
+    rarity: "Common",
+    unlockStreak: 0,
+  },
 
-    tagline: "Your loyal streak companion.",
+  {
+    id: "explorer",
+    name: "Explorer Streaksaur",
+    image: "../img/avatars/streaksaur/explorer.png",
+    collection: "starter",
+    rarity: "Common",
+    unlockStreak: 0,
   },
 
   {
     id: "warrior",
-    name: "Battlesaur",
+    name: "Battle Streaksaur",
     image: "../img/avatars/streaksaur/warrior.png",
-
     collection: "streak",
     rarity: "Rare",
-    class: "Warrior",
+    unlockStreak: 7,
+  },
 
-    unlockType: "streak",
-    unlockValue: 30,
+  {
+    id: "knight",
+    name: "Knight Streaksaur",
+    image: "../img/avatars/streaksaur/knight.png",
+    collection: "streak",
+    rarity: "Rare",
+    unlockStreak: 14,
+  },
 
-    tagline: "Forged through unwavering consistency.",
+  {
+    id: "detective",
+    name: "Detective Streaksaur",
+    image: "../img/avatars/streaksaur/detective.png",
+    collection: "streak",
+    rarity: "Rare",
+    unlockStreak: 21,
+  },
+
+  {
+    id: "pirate",
+    name: "Pirate Streaksaur",
+    image: "../img/avatars/streaksaur/pirate.png",
+    collection: "streak",
+    rarity: "Epic",
+    unlockStreak: 30,
   },
 
   {
     id: "mage",
-    name: "Mysticsaur",
+    name: "Mystic Streaksaur",
     image: "../img/avatars/streaksaur/mage.png",
-
     collection: "streak",
     rarity: "Epic",
-    class: "Mage",
+    unlockStreak: 45,
+  },
 
-    unlockType: "streak",
-    unlockValue: 60,
+  {
+    id: "hero",
+    name: "Hero Streaksaur",
+    image: "../img/avatars/streaksaur/hero.png",
+    collection: "streak",
+    rarity: "Epic",
+    unlockStreak: 60,
+  },
 
-    tagline: "Wisdom grows with every completed habit.",
+  {
+    id: "dragon",
+    name: "Dragon Streaksaur",
+    image: "../img/avatars/streaksaur/dragon.png",
+    collection: "legendary",
+    rarity: "Legendary",
+    unlockStreak: 90,
+  },
+
+  {
+    id: "fire",
+    name: "Fire Streaksaur",
+    image: "../img/avatars/streaksaur/fire.png",
+    collection: "legendary",
+    rarity: "Legendary",
+    unlockStreak: 120,
+  },
+
+  {
+    id: "king",
+    name: "King Streaksaur",
+    image: "../img/avatars/streaksaur/king.png",
+    collection: "legendary",
+    rarity: "Legendary",
+    unlockStreak: 180,
+  },
+
+  {
+    id: "robot",
+    name: "Robot Streaksaur",
+    image: "../img/avatars/streaksaur/robot.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 240,
+  },
+
+  {
+    id: "astronaut",
+    name: "Astronaut Streaksaur",
+    image: "../img/avatars/streaksaur/astronaut.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 300,
+  },
+
+  {
+    id: "batman",
+    name: "Dark Streaksaur",
+    image: "../img/avatars/streaksaur/batman.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 365,
+  },
+
+  {
+    id: "hacker",
+    name: "Cyber Streaksaur",
+    image: "../img/avatars/streaksaur/hacker.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 500,
+  },
+
+  {
+    id: "ninja",
+    name: "Ninja Streaksaur",
+    image: "../img/avatars/streaksaur/ninja.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 650,
+  },
+
+  {
+    id: "saiyan",
+    name: "Saiyan Streaksaur",
+    image: "../img/avatars/streaksaur/saiyan.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 800,
+  },
+
+  {
+    id: "samurai",
+    name: "Samurai Streaksaur",
+    image: "../img/avatars/streaksaur/samurai.png",
+    collection: "master",
+    rarity: "Mythic",
+    unlockStreak: 1000,
   },
 ];
