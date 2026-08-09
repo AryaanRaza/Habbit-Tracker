d089e76f3610c90644186ea72b681ce612477285
MD ADNAAN <devadnaan1611@gmail.com>
2026-08-09 17:53:07 +0530

    feat(pwa): add service worker cache configuration

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "habitflow-v1";
+
+const urlsToCache = [
+  "index.html",
+  "dashboard.html",
+  "analytics.html",
+  "settings.html",
+  "css/base/reset.css",
+  "css/base/root.css",
+  "css/global.css",
+  "css/components/buttons.css",
+  "css/components/navigation.css",
+  "css/pages/dashboard.css",
+  "css/pages/stats.css",
+  "js/core/storage.js",
+  "js/shared/habit-utils.js",
+  "js/shared/navigation.js",
+  "js/dashboard/dashboard-data.js",
+  "img/streakasaur-hero.png",
+];
