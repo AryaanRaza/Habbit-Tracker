ce2ac7e8c5c6ed246d7b3d9d9ab9b1a0b2b15bc6
MD ADNAAN <devadnaan1611@gmail.com>
2026-08-10 17:04:52 +0530

    feat(pwa): cache app resources during install

--- service-worker.js
+++ service-worker.js
@@ -18,3 +18,9 @@ const urlsToCache = [
   "js/dashboard/dashboard-data.js",
   "img/streakasaur-hero.png",
 ];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
